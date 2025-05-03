9ed510b68cb6ad744bbe3222ae619a9655e9a71a
prabhakar <prabhakarpandey7061@gmail.com>
2025-05-03 13:51:33 +0530

    add user model

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,16 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async(req,res)=>{
-    res.status(200).json({
-        message:"ok"
-    })
+  //get user details from frintend
+//   validation  - not emport 
+//check if user aleeady exists:username , email
+//check for images , check for avatar 
+//upload them to cloudinary, avatar
+//create password and refresh token field response
+//remove passwoed and refresh token fied from response
+//check for user creation 
+//return res
+
 })
 
 
---
+++ src/models/user.model.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const usermodel=new mongoose(
+    {
+        usersName:{
+            required:true,
+            types:String,
+            uniqe:true,
+            lowercacs:true,
+            trim:true,
+            index:true,
+
+        },
+        email:{
+            required:true,
+            types:String,
+            uniqe:true,
+            lowercacs:true,
+            trim:true,
+            index:trues
+
+        },
+        fullName:{
+            type:String,
+            required:true,
+            trim:true,
+            index:true
+        },
+        avater:{
+            required:true,
+            types:String,
+
+        },
+        coverImge:{
+            types:String,
+
+        },
+        watchHistory:{
+            types:String,
+
+        },
+        password:{
+            types:String,
+            required:[true,'password is required']
+
+        },
+        refreshToken:{
+            types:String,
+
+
+        }
+    }
+)
